d7abdc749e5e449dace5e27f6bf724791c0ff031
mindon <mindon@gmail.com>
2014-11-09 11:09:49 +0800

    Site updated at 2014-11-09 03:09:49 UTC

---
+++ javascripts/mindon-tags.js
@@ -0,0 +1,44 @@
+var keyLinks = {};
+function tagLink( key ) {
+  var ikey = key.toLowerCase();
+  return '<a href="' + (keyLinks[ikey] || ('http://www.google.com/cse?cx=partner-pub-6997921015773263:4467526896&ie=UTF-8&q=' +encodeURIComponent(ikey))) +'">' + key  +'</a>';
+}
+
+function linkTags( tags ) {
+  if( !tags )
+    return;
+  var t = [], i, v, bb = /^[\u0000-\u00ff]/, be = /[\u0000-\u00ff]$/;
+  for(var k=0, kmax=tags.length; k<kmax; k++) {
+    v = tags[k];
+    i = v.indexOf('=');
+    var key = '';
+    if( i > 0 ) {
+      key = v.substr(0, i).toLowerCase();
+      keyLinks[key] = v.substr( i+1 );
+    } else if( i != 0 ) {
+      key = v;
+    }
+    if( key ) {
+      t.push( (bb.test(key)?'\\b':'') +key +(be.test(key)?'\\b':'') );
+    }
+  }
+
+  var trxp = new RegExp(t.join('|'), 'gi')
+    , tl = '<';
+  t = null;
+
+  var pt = /(a|code|pre|xmp|script|textarea|select|embed|object)/i;
+  $('.entry-content').find('h1,h2,p,div,span,b,li').each(function() {
+    if( this.className != 'adpost' && !pt.test(this.parentNode.tagName) && this.innerHTML ) {
+      if( this.innerHTML.indexOf(tl) < 0 ) {
+        this.innerHTML = this.innerHTML.replace(trxp, tagLink);
+      }
+    }
+  });
+}
+
+if( $('#link-tags').length > 0 ) {
+  var tags = $('#link-tags').html().match(/([^;=]+((=[^;]*)|(?=;)|$))/g);
+  $('#link-tags').remove();
+  linkTags( tags );
+}
---
+++ kit/deki.js
@@ -0,0 +1,83 @@
+(window.deKi && deKi()) || (function() {
+if( window.location.href.indexOf('https://www.amazon.com/gp/digital/fiona/manage') !== 0 ) {
+  alert( 'Only works in https://www.amazon.com/gp/digital/fiona/manage' );
+  return window.open("https://www.amazon.com/gp/digital/fiona/manage");
+}
+
+function _dkiOb(id){return document.getElementById(id)}
+
+var prefix = '_dki_', css = ' style="cursor:pointer;padding:0 6px; display:inline-block;margin-left:6px;background:<bgc>;"';
+
+window.deKi = function(flag) {
+  var t = _dkiOb(prefix +'dekiArea')
+    , hidden = t.style.display == 'none' || flag === true;
+  t.style.display = hidden ? 'block' : 'none';
+
+  if( hidden ) {
+    _dkiOb(prefix+'confirm').style.display = 'none';
+    _dkiOb(prefix+'del').style.display = 'block';
+    _dkiOb(prefix +'word').focus();
+  }
+  t = null;
+  return true; //!
+}
+
+var delKeys = [];
+
+function del(n){
+  delKeys = [];
+  if( n )
+    n = n.toLowerCase();
+
+  var a=document.getElementsByClassName('rowHeaderCollapsed');
+  for(var i = 0; i<a.length; i++){
+   if (!n || a[i].getElementsByClassName('headerTitle')[0].textContent.toLowerCase().indexOf(n) > -1) {
+      var id = a[i].getElementsByTagName('input')[1].value;
+      delKeys.push(id);
+    }
+  }
+};
+
+var d = document.createElement('div');
+d.id = prefix+'dekiArea';
+d.style.cssText = 'display:none;border:8px solid #66A0D6;background:#fff;width:360px;position:fixed;right:16px;top:16px;border-radius:8px;z-index:999999;-webkit-box-shadow:0 7px 8px rgba(128,128,128,0.9);';
+d.innerHTML = '<div style="margin:16px"><a id="'+prefix+'close"'+css.replace(/<bgc>/,'#c30;color:#fff;border-radius:3px;')+'>×</a> Filter: <input id="'+prefix+'word" placeholder="Search your library" style="width:230px"/></div><div style="display:block;line-height:3em;background: #66A0D6;color:#fff;text-align:center;" id="'+prefix+'del"><b>DeKi</b>: Delete Kindle Items <a id="'+prefix+'start"'+css.replace(/<bgc>/,'#fe0;color:#66A0D6')+'>Delete</a></div><div style="line-height:3em;display:none;background:#66A0D6;color:#fff;text-align:center;" id="'+prefix+'confirm">Delete <b id="'+prefix+'count"></b> Item(s) Permanently? <a'+css.replace(/<bgc>/,'#ccc;color:#fff')+' id="'+prefix+'no">No</a> <a'+css.replace(/<bgc>/,'#fe0;color:#66A0D6')+' id="'+prefix+'yes">Yes</a></div><p align="center" style="margin:2px 0;opacity:0.2"><b>DeKi</b> by <a href="http://mindon.github.com">http://mindon.github.com</a></p>';
+document.body.appendChild( d );
+_dkiOb(prefix +'close').onclick = function(){
+  _dkiOb(prefix +'dekiArea').style.display = 'none';
+  _dkiOb(prefix+'confirm').style.display = 'none';
+  _dkiOb(prefix+'del').style.display = 'block';
+};
+d = null;
+
+_dkiOb(prefix +'word').oninput = function() {
+  _dkiOb('searchTextId').value = this.value;
+  _dkiOb('searchTextGoButton').click();
+
+  if( _dkiOb(prefix+'confirm').style.display != 'none' ) {
+    _dkiOb(prefix+'confirm').style.display = 'none';
+    _dkiOb(prefix+'del').style.display = 'block';
+  }
+};
+_dkiOb(prefix+'start').onclick = function(){
+  del();
+
+  _dkiOb(prefix+'count').innerHTML = /^\s*$/.test( _dkiOb(prefix+'word').value ) ? 'All('+delKeys.length+')' : delKeys.length;
+
+  _dkiOb(prefix+'del').style.display = 'none';
+  _dkiOb(prefix+'confirm').style.display = 'block';
+
+};
+
+_dkiOb(prefix+'no').onclick = function(){
+  _dkiOb(prefix+'confirm').style.display = 'none';
+  _dkiOb(prefix+'del').style.display = 'block';
+};
+
+_dkiOb(prefix+'yes').onclick = function(){
+  deKi(true);
+  for(var i = 0; i<delKeys.length; i++){Fion.deleteItem('deleteItem_'+delKeys[i]);}  
+};
+
+deKi();
+})();
